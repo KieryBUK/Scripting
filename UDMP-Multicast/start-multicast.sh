9fb1e2313f36b29b5b35306f7f39ac71af78fdd8
KieryBUK <34575617+KieryBUK@users.noreply.github.com>
2020-07-30 15:33:36 +0100

    Update start-multicast.sh

--- UDMP-Multicast/start-multicast.sh
+++ UDMP-Multicast/start-multicast.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-## configuration variables:
+## Start PodMan with the multi-cast realy container
+podman run -d --restart=always --network=host -e OPTS="--noMDNS" -e INTERFACES="br0 br107" docker.io/scyto/multicast-relay
