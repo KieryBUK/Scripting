f0453389fa1b35e4050a72713c18316c85daf975
KieryBUK <34575617+KieryBUK@users.noreply.github.com>
2020-07-30 16:48:42 +0100

    Delete 10-start-multicast.sh

--- UDMP-Multicast/10-start-multicast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if podman container exists multicastrelay; then
-  podman start multicastrelay
-else
-  podman container create --name multicastrelay -d --restart=always --network=host -e OPTS="--noMDNS" -e INTERFACES="br0 br99" docker.io/scyto/multicast-relay
-  podman start multicastrelay
-fi
