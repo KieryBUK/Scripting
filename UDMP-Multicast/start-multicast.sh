5c74391ff00b32f88a50bb751cb712c97f625cde
KieryBUK <34575617+KieryBUK@users.noreply.github.com>
2020-07-30 16:16:19 +0100

    Update start-multicast.sh

--- UDMP-Multicast/start-multicast.sh
+++ UDMP-Multicast/start-multicast.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run -d --restart=always --network=host -e OPTS="--noMDNS" -e INTERFACES="br0 br107" docker.io/scyto/multicast-relay
+podman run -d --restart=always --network=host -e OPTS="--noMDNS" -e INTERFACES="br0 br107" /mnt/data/on_boot.d/multicast-relay
