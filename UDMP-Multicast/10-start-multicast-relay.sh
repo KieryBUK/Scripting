3ee38cb54e1076c0fd016316892d3a3e0b2299b2
KieryBUK <34575617+KieryBUK@users.noreply.github.com>
2020-07-30 17:12:09 +0100

    Update 10-start-multicast-relay.sh

--- UDMP-Multicast/10-start-multicast-relay.sh
+++ UDMP-Multicast/10-start-multicast-relay.sh
@@ -3,6 +3,6 @@
 if podman container exists multicastrelay; then
   podman start multicastrelay
 else
-  podman container create --name multicastrelay -d --restart=always --network=host -e OPTS="--noMDNS" -e INTERFACES="br0 br99" docker.io/scyto/multicast-relay
+  podman container create --name multicastrelay -d --restart=always --network=host -e OPTS="--noMDNS" -e INTERFACES="br0 br107" docker.io/scyto/multicast-relay
   podman start multicastrelay
 fi
